f5400e23cf9047249b7bb76e7b296f417b9bac2d
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-09-11 19:04:09 +0100

    creat'

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/MainContent'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <WelcomeMessage />
+     <Header />
+     <Footer />
+     <Main />
     </>
   )
 }
