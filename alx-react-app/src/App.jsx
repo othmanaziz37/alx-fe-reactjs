a88b41b39035184ed357cf35a537bb0d6bd5d5df
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-09-12 16:49:57 +0100

    creat

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
