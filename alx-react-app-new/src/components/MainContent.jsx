45a4bc225075f69297e821cfc8efb537460e451e
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-09-12 18:54:21 +0100

    creat

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent() {
     return (
       <main>
-          <p style={{ fontWeight: 'bold' }}>I love to visit New York, Paris, and Tokyo.</p>
+          <p style={{ fontWeight: 'bold' }}> I love to visit New York, Paris, and Tokyo.</p>
       </main>
     );
   }
