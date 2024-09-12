77084a4235e17ce7f1f923eec4a946d5317d15d0
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-09-12 16:58:42 +0100

    creat

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,6 +16,7 @@ function App() {
      <WelcomeMessage />
      <Header />
      <MainContent />
+     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
      <Footer />
     
     </>
