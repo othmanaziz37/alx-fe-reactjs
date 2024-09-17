073b6ed658b74bd42a89447868d379c8b119fbef
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-09-17 21:48:28 +0100

    cteat

--- alx-react-app-props/src/Components/UserContext.js
+++ alx-react-app-props/src/Components/UserContext.js
@@ -1,6 +1,5 @@
-import React from "react";
-import { createContext } from "react";
+import React from 'react';
 
-const UserContext = createContext({});
+const UserContext = React.createContext();
 
 export default UserContext;
