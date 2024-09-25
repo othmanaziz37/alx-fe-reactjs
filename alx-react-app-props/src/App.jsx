3c7bec6956e5b900903e4a01c62808bb2eda6582
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-09-25 17:10:25 +0100

    craet

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import ProfilePage from './ProfilePage';
 import UserContext from './Components/UserContext.js';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
+  return(
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  )
 }
 
 export default App;
--- alx-react-app-props/src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header () {
-    return (
-        <header>
-            <h1>My Favourite City </h1>
-        </header>
-    )
-}
-
-export default Header;
