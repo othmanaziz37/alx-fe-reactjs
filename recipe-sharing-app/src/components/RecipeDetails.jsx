013125f6c6f9a0de9a652768ffbae0c58031c380
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-09-26 17:14:10 +0100

    creat

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,28 +1,32 @@
+// RecipeDetails component
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
-import { Link, useParams } from 'react-router-dom';
-import DeleteRecipeButton from './DeleteRecipeButton';
 import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
+import FavBtn from './FavBtn';
 
-  const RecipeDetails = () => {
-
-    const {id} = useParams();
+const RecipeDetails = () => {
+    const { recipeId } = useParams(); 
+    const navigate = useNavigate();
     const recipe = useRecipeStore(state =>
-      state.recipes.find(recipe => recipe.id === parseInt(id))
+      state.recipes.find(recipe => recipe.id === parseInt(recipeId))
     );
 
-    if (!recipe) {
-        return <div>Recipe Not Found</div>;
-    }
+  if (!recipe) {return <div>Something is wrong! - Recipe not found.</div>;}
 
-    return (
-      <div>
-        <Link to='/'>Home</Link>
-        <h1>{recipe.title}</h1>
-        <p>{recipe.description}</p>
-        <EditRecipeForm recipe={recipe}/>
-        <DeleteRecipeButton recipe={recipe} />
-      </div>
-    );
-  };
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <FavBtn recipeId={recipe.id} />
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipeId} />
+      <button onClick={() => navigate('/')} style={{ backgroundColor: 'blue', color: 'white', cursor: 'pointer' }}>
+        Back to Home
+      </button>
+    </div>
+  );
+};
 
-export default RecipeDetails
+export default RecipeDetails;
