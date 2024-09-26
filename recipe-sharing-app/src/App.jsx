b02f34c1abd5a2ab4c2f08fa161218e8234840dc
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-09-26 17:10:05 +0100

    creat

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,25 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
-import Recommendations from './components/RecommendationsList';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import RecipeDetails from './components/RecipeDetails'
+import './App.css'
+
 
 function App() {
- return (
+
+  return (
     <Router>
-    <div className="App">
-      <h1>Recipe Sharing App</h1>
-      <SearchBar /> 
-      <AddRecipeForm />
-      <Recommendations />
       <Routes>
         <Route path="/" element={<RecipeList />} />
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
       </Routes>
-    </div>
-  </Router>
- );
+    </Router>
+  )
 }
 
-export default App;
+export default App
