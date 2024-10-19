06e0c1882fbf782767bfdf5b7e67357c7ed311a9
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-10-19 22:47:53 +0100

    creat

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -1,47 +1,86 @@
-import React from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Data from '../data.json';
+import React, { useState }from 'react'
 
-const RecipeDetail = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [recipe, setRecipe] = useState(null);
+const AddRecipeForm = () => {
+  const [fields, setFields] = useState({
+    title: '',
+    summary: '',
+    ingredients: '',
+    preparation: ''
+  });
 
-    useEffect(() => {
-        const recipe = Data.find((recipe) => recipe.id === Number(id));
-        setRecipe(recipe);
-    }, [id]);
+  const [errors, setErrors] = useState({})
 
-    if (!recipe) {
-        return <div>Recipe not found</div>;
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFields({
+      ...fields, [name]:e.target.value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const validateFields = {}
+    if(!fields.title.trim()) {
+      validateFields.title = "Recipe title is required"
+    } if (!fields.summary.trim()) {
+      validateFields.summary = "Recipe summary is required"
+    } 
+    
+    if (!fields.ingredients.trim()) {
+      validateFields.ingredients = "Recipe ingredients are required"
+    } else {
+      const ingredientsArray = fields.ingredients.split(',')
+      .map(i => i.trim())
+      .filter(i => i !== "");
+
+      if(ingredientsArray.length < 3) {
+        validateFields.ingredients = "Recipe ingredients is below 3 items"
+      }
     }
+    
+    if (!fields.preparation.trim()) {
+      validateFields.preparation = "Preparation steps are required"
+    } 
+      setErrors(validateFields)
+      console.log('This errors:', validateFields);
+      console.log('Results:', fields);
 
-    return (
-        <div className="flex justify-center items-center h-screen bg-gray-100">
-            <div className="max-w-lg w-full p-6 bg-white rounded-lg shadow-md">
-                <img src={recipe.image} alt={recipe.title} className="w-full h-auto rounded-lg" />
-                <h2 className="text-3xl md:text-4xl font-medium text-blue-900 mt-4">{recipe.title}</h2>
-                <p className="text-lg md:text-2xl text-blue-900 mt-2">{recipe.summary}</p>
-                <div className="mt-4">
-                    <h3 className="text-xl md:text-2xl font-medium text-blue-900">Ingredients:</h3>
-                    <ul className="list-disc list-inside text-blue-900 mt-2">
-                        {recipe.ingredients.map((ingredient, index) => (
-                            <li key={index}>{ingredient}</li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="mt-4">
-                    <h3 className="text-xl md:text-2xl font-medium text-blue-900">Preparation Steps:</h3>
-                    <ol className="list-decimal list-inside text-blue-900 mt-2">
-                        {recipe.instructions.map((step, index) => (
-                            <li key={index}>{step}</li>
-                        ))}
-                    </ol>
-                </div>
-            </div>
-        </div>
-    );
-};
+    if(Object.keys(validateFields).length === 0) {
+      alert('Form Sent');
+    }
+  }
 
-export default RecipeDetail;
+  return (
+    <>
+    <h1 className='pl-8 mt-8 text-green-950'>ADD A NEW RECIPE</h1>
+    <form onSubmit={handleSubmit} className='font-medium flex flex-col gap-2 mx-6 mb-16 p-8 w-[400px] md:w-[600] border-solid border-blue-900 rounded shadow-gray-600'>
+      <label htmlFor="title">Recipe title</label>
+      <input className='border-solid border-2 border-slate-200 pl-2 leading-7' type="text" name='title' placeholder='Enter recipe name'
+      onChange={handleChange}/>
+      {errors.title && <span className='text-red-600 text-xs'>{errors.title}</span>}
+
+      <label className='mt-4' htmlFor="summary"></label> Recipe summary
+      <input className='border-solid border-2 border-slate-200 pl-2 leading-7'  type="text" name="summary" placeholder="Enter short description" 
+      onChange={handleChange}/>
+      {errors.summary && <span className='text-red-600 text-xs'>{errors.summary}</span>}
+
+      <label className='mt-4' htmlFor="recipeimage">Upload image</label>
+      <input className='' type="file" name="recipeimage" />
+
+      <label className='mt-4' htmlFor="ingredients">Recipe ingredients</label>
+      <textarea className='border-solid border-2 border-slate-200 pl-2 leading-7 '  name="ingredients" cols="40" rows="10" 
+      onChange={handleChange}/>
+      {errors.ingredients && <span className='text-red-600 text-xs'>{errors.ingredients}</span>}
+
+      <label className='mt-4' htmlFor="preparation">Recipe preparation</label>
+      <textarea className='border-solid border-2 border-slate-200 pl-2 leading-7'  name="preparation" cols="40" rows="10" 
+      onChange={handleChange}/>
+      {errors.preparation && <span className='text-red-600 text-xs'>{errors.preparation}</span>}
+
+      <button className='bg-lime-800 py-2 mt-2 text-white mt-8' type="submit">Submit</button>
+    </form>
+    </>
+  )
+}
+
+export default AddRecipeForm
