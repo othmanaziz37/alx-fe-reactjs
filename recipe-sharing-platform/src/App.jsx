7607d880bfb893802e8d8a397b071090d3449c4d
othmanaziz37 <150553298+othmanaziz37@users.noreply.github.com>
2024-10-19 22:41:32 +0100

    creaaat

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import HomePage from './components/HomePage'
+import AddRecipeForm from '../../recipe-sharing-app/src/components/AddRecipeForm'
+import RecipeDetail from './components/RecipeDetail'
 
 function App() {
   const [count, setCount] = useState(0)
